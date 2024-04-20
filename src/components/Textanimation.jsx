ffb513cdc73516f6f0832faaa0448ce934c47703
beprodeep4718 <144547279+beprodeep4718@users.noreply.github.com>
2024-04-20 10:56:17 +0530

    done

--- src/components/Textanimation.jsx
+++ src/components/Textanimation.jsx
@@ -7,34 +7,34 @@ import { useGSAP } from "@gsap/react";
 
 
 const Textanimation = ({refer}) => {
-  gsap.registerPlugin(useGSAP);
-  gsap.registerPlugin(SplitText, ScrollTrigger);
+  // gsap.registerPlugin(useGSAP);
+  // gsap.registerPlugin(SplitText, ScrollTrigger);
 
-  useGSAP(
-    () => {
-      // gsap code here...
-      const split = new SplitText(".wrapper p", {
-        type: "chars",
-      });
+  // useGSAP(
+  //   () => {
+  //     // gsap code here...
+  //     const split = new SplitText(".wrapper p", {
+  //       type: "chars",
+  //     });
       
-      gsap.timeline({
-        scrollTrigger: {
-          trigger: "#text-target",
-          start: "top top",
-          end: "+=150%",
-          pin: true,
-          scrub: 0.75,
-          markers: false,
-        }
-      })
-        .set(split.chars, {
-          color: "#000000",
-          stagger: 0.1,
-        }, 0.1);
-    },
-    {scope: refer}
+  //     gsap.timeline({
+  //       scrollTrigger: {
+  //         trigger: "#text-target",
+  //         start: "top top",
+  //         end: "+=150%",
+  //         pin: true,
+  //         scrub: 0.75,
+  //         markers: false,
+  //       }
+  //     })
+  //       .set(split.chars, {
+  //         color: "#000000",
+  //         stagger: 0.1,
+  //       }, 0.1);
+  //   },
+  //   {scope: refer}
     
-  );
+  // );
 
   return (
     <>
@@ -45,7 +45,7 @@ const Textanimation = ({refer}) => {
           <section id="textSection" className="p-5">
             <div className="container">
               <div className="wrapper">
-                <p className="white text-5xl font-[gilroy] font-light leading-[70px] text-[#8E8E8E]">
+                <p className="white text-5xl font-[gilroy] font-light leading-[70px]">
                   Our Anjusa Academy started its <br /> journey in 2003 and it has been <br />
                   21 long years since then. <br /> Currently, we are constantly <br /> painting the picture of the future with 400+ students.
                 </p>
