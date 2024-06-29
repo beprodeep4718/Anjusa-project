ede94f62d926dc4d583a91581a47beeba2d8284a
beprodeep4718 <144547279+beprodeep4718@users.noreply.github.com>
2024-06-29 10:24:44 +0530

    analytic

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import Body from "./components/Body";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Admin from "./components/Admin";
 import Notfound from "./components/Notfound";
+import 'react-toastify/dist/ReactToastify.css';
+import { inject } from "@vercel/analytics"
 
 const App = () => {
+  inject();
   return (
     <div className="w-full bg-zinc-200 overflow-hidden">
       <Navbar name="bepro" />
--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,12 @@ import App from "./App.jsx";
 import "./index.css";
 import { AuthProvider } from "./store/auth.jsx";
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { Analytics } from "@vercel/analytics/react"
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider>
     <React.StrictMode>
       <App />
-      <Analytics />
       <ToastContainer />
     </React.StrictMode>
   </AuthProvider>
