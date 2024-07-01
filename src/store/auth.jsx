003443a6cf6e6ad50ae0ab0ee8d9374736cbdbfc
beprodeep4718 <144547279+beprodeep4718@users.noreply.github.com>
2024-07-01 19:38:51 +0530

    loader

--- src/store/auth.jsx
+++ src/store/auth.jsx
@@ -1,36 +1,40 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-
-
 export const Authcontext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    //
+  //
 
-    const [notice, setNotice] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const serverUrl = "https://anjusa-backend.onrender.com"
+  const [notice, setNotice] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const serverUrl = "https://anjusa-backend.onrender.com";
 
-    const getNotice = async () => {
-        const response = await fetch(`${serverUrl}/api/auth/notices`);
-        if (!response.ok) {
-            throw new Error("Failed to get notices");
-        }
-        const data = await response.json();
-        setLoading(false);
-        setNotice(data);
+  const getNotice = async () => {
+    try {
+        setLoading(true);
+      const response = await fetch(`${serverUrl}/api/auth/notices`);
+      if (!response.ok) {
+        throw new Error("Failed to get notices");
+      }
+      const data = await response.json();
+      setNotice(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
     }
+  };
 
-    useEffect(() => {
-        getNotice();
-    })
-    return (
-        <Authcontext.Provider value={{notice, serverUrl, loading}}>
-            {children}
-        </Authcontext.Provider>
-    )
-}
+  useEffect(() => {
+    getNotice();
+  }, []);
+  return (
+    <Authcontext.Provider value={{ notice, serverUrl, loading }}>
+      {children}
+    </Authcontext.Provider>
+  );
+};
 
 export const useAuth = () => {
-    return useContext(Authcontext);
-}
+  return useContext(Authcontext);
+};
