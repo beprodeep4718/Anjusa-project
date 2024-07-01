5d0adcc4576ec7ad3201230d610ae53ed453f208
beprodeep4718 <144547279+beprodeep4718@users.noreply.github.com>
2024-07-01 20:24:15 +0530

    done

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -35,6 +35,7 @@ const Body = () => {
               </div>
             );
           })}
+          
         </ol>
       </section>
       <div>
--- src/store/auth.jsx
+++ src/store/auth.jsx
@@ -11,12 +11,17 @@ export const AuthProvider = ({ children }) => {
 
   const getNotice = async () => {
     try {
-        setLoading(true);
+      setLoading(true);
       const response = await fetch(`${serverUrl}/api/auth/notices`);
       if (!response.ok) {
         throw new Error("Failed to get notices");
       }
       const data = await response.json();
+      setLoading(false);
+      if(!data) {
+        setNotice([{desc: "No notices found"}]);
+        return;  // Return early if no data is received. This prevents unnecessary API calls.  // This is a good practice to prevent potential bugs.
+      }
       setNotice(data);
     } catch (error) {
       console.log(error);
